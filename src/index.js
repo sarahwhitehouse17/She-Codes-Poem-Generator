042599f7e98eeb2f39a95ee52f53276bba0e3215
sarahwhitehouse17 <sarahwhitehouse81@gmail.com>
2024-10-25 12:02:54 +0200

    Improved User Experience
    Hint section, and blinking animation

--- src/index.js
+++ src/index.js
@@ -11,12 +11,15 @@ function generatePoem(event) {
   event.preventDefault();
   userInput = document.querySelector("#user-input-for-poem");
   let apiKey = "3deb07c475cdt015b4f399a253e4o0b8";
-  let prompt = `Write a poem about ${userInput.value}`;
+  let prompt = `Write a short poem about ${userInput.value}, make sure to include four separate lines, and wrap in a <p> HTML format`;
   let context =
-    "You are an experienced poet. Make sure to write a short poem, with a maximum of 2 lines. Please make sure 2 lines are displayed, one on top of the other. Sign at the end with 'She Codes AI' in <strong> element, and put this on a line underneath the poem. Have a new line for the each capital letter";
+    "You are an experienced poet. Make sure to write a short poem, with a maximum of 4 lines. Please make sure each line is displayed as a separate line, meaning 4 lines are visible (one underneath the other). Sign at the end with 'She Codes AI' in <strong> element, and put this on a line underneath the poem.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   console.log("Searching for a poem");
   axios.get(apiUrl).then(displayPoem);
+  let hiddenPoem = document.querySelector("#poem");
+  hiddenPoem.classList.remove("hidden");
+  hiddenPoem.innerHTML = `⏳ <div class = "generating">Generating poem about ${userInput.value}...</div>`;
 }
 
 poemElement = document.querySelector("#poem-generator-form");
